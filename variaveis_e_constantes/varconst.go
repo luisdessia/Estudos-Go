1336c8f39ecc24910cb11ccecec54d3c21950d5b
luisdessia <luisdessia@gmail.com>
2025-01-23 09:13:13 -0300

    Variáveis e Constantes em Go

---
+++ variaveis_e_constantes/varconst.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main (){
+
+	//Váriaveis
+	var nome string
+	nome = "Bento"
+	fmt.Println(nome)
+
+	nome = "Steph"
+	fmt.Println(nome)
+
+	var a = "steph"
+	fmt.Println(a)
+
+	var b,c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+	numero := 1
+	fmt.Println(numero)
+
+	//constantes 
+	const idadeBento = 4
+	fmt.Println(idadeBento)
+}
