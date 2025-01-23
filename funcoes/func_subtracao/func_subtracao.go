d1cfba5efc37fb79dda11b9f8f6c1f2cc6ac4348
luisdessia <luisdessia@gmail.com>
2025-01-23 20:11:02 -0300

    Exemplo de Função de Subtração em Go

---
+++ funcoes/func_subtracao/func_subtracao.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println(subtracao(10, 5))
+	
+	//em go dá para atribuir uma função a uma variável
+	Valor_subtraido := subtracao(20, 12)
+
+	fmt.Println(Valor_subtraido)
+
+}
+
+
+func subtracao (x int, y int) int {
+
+	return x - y
+}
